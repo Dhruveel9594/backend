86632de89b4721994931f8e094f067516acdd6cf
Dhruveel9594 <dhruveeldevaliya@gmail.com>
2025-10-05 21:05:10 +0530

    all setup

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,7 @@
 const asyncHandler = (requestHandler) => {
     // Return a new function that Express can use as a route handler
 
-    (req,res,next) => {
+    return (req,res,next) => {
         // Call the original requestHandler and wrap it in Promise.resolve()
         // This ensures that both sync and async functions are handled as Promises
         Promise.resolve(requestHandler(req,res,next))// If the Promise is rejected (i.e., an error occurs), catch it
